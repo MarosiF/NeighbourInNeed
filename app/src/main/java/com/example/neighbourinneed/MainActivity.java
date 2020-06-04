7e0bec9fb0de4c59dd1a62a9219979b49dd8b48b
MarosiF <49545584+MarosiF@users.noreply.github.com>
2020-06-04 16:18:00 +0200

    Regsiter- Database Test
    - Layout
    - Database test

--- app/src/main/java/com/example/neighbourinneed/MainActivity.java
+++ app/src/main/java/com/example/neighbourinneed/MainActivity.java
@@ -3,23 +3,38 @@ package com.example.neighbourinneed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button RegisterSubmitButton;
+    private EditText InputName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-
-        myRef.setValue("Hello, World!");
+        setContentView(R.layout.register);
 
 
+        RegisterSubmitButton = (Button) findViewById(R.id.button);
+        InputName = (EditText) findViewById(R.id.editText);
+
+
+        RegisterSubmitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                CreateAccount();
+            }
+        });
     }
+
+    private void CreateAccount(){
+        String name = InputName.getText().toString();
+    }
+
 
 }
