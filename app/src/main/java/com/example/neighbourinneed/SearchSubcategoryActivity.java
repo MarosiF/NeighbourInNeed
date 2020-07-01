b553061664dc30717a185976f3278351e1f2a551
Ebrulaleli <49160195+Ebrulaleli@users.noreply.github.com>
2020-07-01 21:58:30 +0200

    implemented RecyclerView in order to retrieve and display dynamic list of advertisements from the db

--- app/src/main/java/com/example/neighbourinneed/SearchSubcategoryActivity.java
+++ app/src/main/java/com/example/neighbourinneed/SearchSubcategoryActivity.java
@@ -1,14 +1,93 @@
 package com.example.neighbourinneed;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SearchSubcategoryActivity extends AppCompatActivity {
+    DatabaseReference ref;
+    ArrayList<Advertisement> list;
+    RecyclerView recyclerView;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_subcategory);
+
+        ref = FirebaseDatabase.getInstance().getReference().child("Advertisement");
+        recyclerView = findViewById(R.id.recyclerView);
+        searchView = findViewById(R.id.searchView);
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+
+        if (ref != null) {
+
+            ref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        list = new ArrayList<Advertisement>();
+                        for(DataSnapshot ds : dataSnapshot.getChildren()) {
+                            list.add(ds.getValue(Advertisement.class));
+                        }
+                        AdapterClass adapterClass = new AdapterClass(list);
+                        recyclerView.setAdapter(adapterClass);
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Toast.makeText(SearchSubcategoryActivity.this, databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        if (searchView != null) {
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    search(newText);
+                    return false;
+                }
+            });
+
+        }
+    }
+
+
+    private void search(String s) {
+        ArrayList<Advertisement> myList = new ArrayList<Advertisement>();
+
+        for (Advertisement ad : list) {
+            if (ad.getDescription().toLowerCase().contains(s.toLowerCase())) {
+                myList.add(ad);
+
+            }
+        }
+
+        AdapterClass adapterClass = new AdapterClass(myList);
+        recyclerView.setAdapter(adapterClass);
     }
 }
