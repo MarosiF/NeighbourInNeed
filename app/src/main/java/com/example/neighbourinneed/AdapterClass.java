6599b8a8882bd91267d96f14a9d8443c89bb5f35
Ebrulaleli <49160195+Ebrulaleli@users.noreply.github.com>
2020-07-01 22:03:16 +0200

    implemented AdapterClass for RecyclerView

---
+++ app/src/main/java/com/example/neighbourinneed/AdapterClass.java
@@ -0,0 +1,51 @@
+package com.example.neighbourinneed;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+public class AdapterClass extends  RecyclerView.Adapter<AdapterClass.MyViewHolder> {
+    ArrayList<Advertisement> list;
+
+    public AdapterClass(ArrayList<Advertisement> list) {
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ad_holder, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.id.setText(list.get(position).getName());
+        holder.desc.setText(list.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.list.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView id, desc;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            id = itemView.findViewById(R.id.title);
+            desc = itemView.findViewById(R.id.description);
+        }
+    }
+}
